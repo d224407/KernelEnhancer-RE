1cc23dced22fa003cbe143e93787de48ba263f26
d224407 <nguynlanh63@gmail.com>
2026-07-04 10:29:15 +0700

    Update action.sh

--- action.sh
+++ action.sh
@@ -2,13 +2,6 @@
 
 MODDIR=/data/adb/modules/$(basename $(dirname $0))
 
-# Đợi một chút để hệ thống ổn định
-sleep 2
-
-# Chạy script tối ưu kernel
 sh $MODDIR/kernelenhance.sh
-
 echo "Manual optimize completed at $(date)" >> /data/local/tmp/status.log
-
-# Thông báo trên màn hình (nếu có Magisk Manager UI)
-ui_print "✅ Kernel optimized successfully!"
+ui_print "✅ Kernel & Touch Optimized!"
