5da6548ecfb3dd675b93c062029d32af85da079c
d224407 <nguynlanh63@gmail.com>
2026-08-13 19:14:24 +0700

    Fix x86

--- template/customize.sh
+++ template/customize.sh
@@ -9,13 +9,13 @@ set_perm_recursive $MODPATH 0 0 0755 0644
 set_perm $MODPATH/system/bin/kernelenhancer_32 0 0 0755
 set_perm $MODPATH/system/bin/kernelenhancer_64 0 0 0755
 set_perm $MODPATH/system/bin/kernelenhancer_x86 0 0 0755
-set_perm $MODPATH/system/bin/kernelenhancer_x86_64 0 0 0755
+set_perm $MODPATH/system/bin/kernelenhancer_x64 0 0 0755
 set_perm $MODPATH/service.sh 0 0 0755
 
 ui_print "- Checking binaries..."
 [ -f "$MODPATH/system/bin/kernelenhancer_32" ] && ui_print "  ARMv7 (32-bit): OK"
 [ -f "$MODPATH/system/bin/kernelenhancer_64" ] && ui_print "  ARMv8 (64-bit): OK"
 [ -f "$MODPATH/system/bin/kernelenhancer_x86" ] && ui_print "  x86 (32-bit): OK"
-[ -f "$MODPATH/system/bin/kernelenhancer_x86_64" ] && ui_print "  x86_64 (64-bit): OK"
+[ -f "$MODPATH/system/bin/kernelenhancer_x64" ] && ui_print "  x86_64 (64-bit): OK"
 
 ui_print "✅ Done!"
